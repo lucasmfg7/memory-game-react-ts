20a40f10d29ea4e5172d2cf29348bdce36ef12d4
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-04-15 20:25:13 -0300

    Setting up and Shuffling Cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import { cardImages, Cards } from "./data/cads";
 
 const App = () => {
+  const [cards, setCards] = useState<Cards[]>([]);
+  const [turns, setTurns] = useState<number>(0);
+
+  function shuffleCards() {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  }
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>Novo Jogo</button>
+      <button onClick={shuffleCards}>Novo Jogo</button>
     </div>
   );
 };
---
+++ src/data/cads.ts
@@ -0,0 +1,12 @@
+export interface Cards {
+  src: string;
+}
+
+export const cardImages: Cards[] = [
+  { src: "../../img/helmet-1.png" },
+  { src: "../../img/potion-1.png" },
+  { src: "../../img/ring-1.png" },
+  { src: "../../img/scroll-1.png" },
+  { src: "../../img/shield-1.png" },
+  { src: "../../img/sword-1.png" },
+];
