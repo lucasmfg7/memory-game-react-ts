0bc7479e827b5e53c6c99ba9dfe78f00a6bf6a5e
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-04-15 20:12:30 -0300

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>Novo Jogo</button>
+    </div>
+  );
+};
+
+export default App;
