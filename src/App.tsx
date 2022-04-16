b8fb78c5915862ced273acb2988d2f10e4ef2481
Lucas Fernandes <lucas_matheus22@hotmail.com>
2022-04-15 21:08:42 -0300

    Making Card Choices

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import "./App.css";
 import { SingleCard } from "./components/SingleCard";
 import { cardImages } from "./data/cads";
 
-export interface Cards {
+export interface Card {
   src: string;
   id: number;
 }
 
 const App = () => {
-  const [cards, setCards] = useState<Cards[]>([]);
+  const [cards, setCards] = useState<Card[]>([]);
   const [turns, setTurns] = useState<number>(0);
+  const [choiceOne, setChoiceOne] = useState<Card>();
+  const [choiceTwo, setChoiceTwo] = useState<Card>();
 
   function shuffleCards() {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -21,6 +23,10 @@ const App = () => {
     setTurns(0);
   }
 
+  function handleChoice(card: Card) {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -28,7 +34,7 @@ const App = () => {
       <div className="card-grid">
         {cards.map((card) => (
           <div key={card.id}>
-            <SingleCard key={card.id} card={card} />
+            <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
           </div>
         ))}
       </div>
--- src/components/SingleCard.tsx
+++ src/components/SingleCard.tsx
@@ -1,15 +1,26 @@
+import { Card } from "../App";
+
 interface CardProps {
   card: {
     src: string;
     id: number;
   };
+  handleChoice: (card: Card) => void;
 }
 
-export const SingleCard = ({ card }: CardProps) => {
+export const SingleCard = ({ card, handleChoice }: CardProps) => {
+  function handleClick() {
+    handleChoice(card);
+  }
   return (
     <div className="card">
       <img className="front" src={card.src} alt="front card" />
-      <img className="back" src="../img/cover.png" alt="back card" />
+      <img
+        className="back"
+        src="../img/cover.png"
+        alt="back card"
+        onClick={handleClick}
+      />
     </div>
   );
 };
